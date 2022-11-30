50b959a8204434d0507bf12f9193175736b2ce2b
root <abelpzhang@tencent.com>
2022-11-30 10:56:13 +0800

    update deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,10 +2,8 @@ tar -zxvf bundle.tar.gz
 tar -zxvf vim9.tar.gz
 mkdir -p ../.local
 mv root/.local/* ../.local
-mv vimrc.mini vimrc
-yum install -y ruby
 cp bin/* ../.local/bin
 echo "export PATH=\"~/.local/bin:\$PATH\"" >> ~/.bashrc
-source ~/.bashrc
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-yum install -y ripgrep
+yum install -y ripgrep ruby
+mv vimrc.mini vimrc
