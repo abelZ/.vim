9db2e553ce65b0b22973df305506bb5e20ea8cc6
abelpzhang <abelpzhang@tencent.com>
2023-12-05 12:44:32 +0800

    fix: fix python build

--- tools/build_python.sh
+++ tools/build_python.sh
@@ -1,4 +1,4 @@
-step1: build openssl
+step1: build openssl, link lib64 to lib because python build search for lib
 step2: download python tar
 step3: yum update & yum install openssl-devel bzip2-devel libffi-devel & yum groupinstall "Development Tools"
 step5: ./configure --enable-optimizations --enable-shared --with-openssl=$SSL_DIR
